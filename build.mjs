0291222adf6e92d6f0d6107e532fab75328d039f
KaMeHb-UA <marlock@etlgr.com>
2018-08-10 01:36:56 +0300

    Built resources moved from /docs folder to root

--- build.mjs
+++ build.mjs
@@ -1,11 +1,9 @@
-import {mkdir, list, copy} from 'components/fs';
+import {list, copy} from 'components/fs';
 
 (async () => {
     // Just for test, copy files without building
-    const src = `src`,
-        build = `docs`;
-    await mkdir(build);
+    const src = `src`;
     (await list(src)).forEach(file => {
-        copy(`${src}/${file}`, `${build}/${file}`)
+        copy(`${src}/${file}`, `${file}`)
     });
 })()
