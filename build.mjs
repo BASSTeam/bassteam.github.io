10c4b869712475b14f224973ae49f0fd02564e23
KaMeHb-UA <marlock@etlgr.com>
2018-08-13 18:18:44 +0300

    saving build hash for pre-push hook
    [hidden] pre-push hook update

--- build.mjs
+++ build.mjs
@@ -1,9 +1,10 @@
-import {list, remove} from 'components/fs';
+import {write, list, remove} from 'components/fs';
 import notFound from './src/404';
 import index from './src/index';
 import app from './src/app';
 import staticjs from './src/static';
 import serviceWorker from './src/service-worker';
+import {createHash} from 'crypto';
 (async () => {
     const sources = [
         // List that prevents deleting source files
@@ -23,5 +24,15 @@ import serviceWorker from './src/service-worker';
         if (sources.indexOf(file) == -1) flist.push(remove(file));
     });
     await Promise.all(flist); // wait until all the files have been removed
-    await Promise.all([notFound(), index(), app(), serviceWorker(), staticjs()])
+    await write('.buildhash', createHash('md5').update(
+        (
+            await Promise.all([
+                notFound(),
+                index(),
+                app(),
+                serviceWorker(),
+                staticjs()
+            ])
+        ).join('')
+    ).digest("hex"))
 })()
