525fc491f8e2dec2ede66a7ccd6a90a5890800d8
KaMeHb-UA <marlock@etlgr.com>
2018-08-26 00:13:41 +0300

    Better types description

--- src/pages/index.d.ts
+++ src/pages/index.d.ts
@@ -1,15 +1,25 @@
 declare const Page = (() => {
-    class Element {
-        constructor(name: String, data: Object){}
+    var ElementData: Symbol;
+    var attrs: Symbol;
+    var childs: Symbol;
+    var content: Symbol;
+    var node: Symbol;
+    interface ElementRefference{
+        [childs]?: Array<Element>;
+        [attrs]?: {[s: String]: String};
+        [content]?: String;
+    }
+    class Element{
+        constructor(name: String, data: ElementRefference){}
     }
     return class Page {
-        constructor(pageRef: { head: Element, body: Element }){}
-        static Element: Element
-        static ElementData: Symbol;
-        static attrs: Symbol;
-        static childs: Symbol;
-        static content: Symbol;
-        static node: Symbol;
+        constructor(pageRef: { head: ElementRefference, body: ElementRefference }){}
+        static Element = Element;
+        static ElementData = ElementData;
+        static attrs = attrs;
+        static childs = childs;
+        static content = content;
+        static node = node;
         head: Element;
         body: Element;
     }
